5a68f9d70c95c3fd42c9e7358e1079da8b741ebf
Vindula <Vindula@DESKTOP-S66AIGF>
2019-12-19 22:13:32 +0530

    Commit 111

---
+++ src/ja3/JA3.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ja3;
+
+/**
+ *
+ * @author Vindula
+ */
+public class JA3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Main ma = new Main();
+       ma.setVisible(true);
+        System.out.println("Hellorrrrrrrr");
+       
+        // TODO code application logic here
+    }
+    
+}
